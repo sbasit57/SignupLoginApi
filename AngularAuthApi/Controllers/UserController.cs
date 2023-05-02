c6f3695b533a13049810e9d9d06700e75d6945a1
Syed Abdul Basit <79485785+sbasit57@users.noreply.github.com>
2023-05-02 17:01:03 +0500

    Add project files.

---
+++ AngularAuthApi/Controllers/UserController.cs
@@ -0,0 +1,48 @@
+﻿using AngularAuthApi.Contexts;
+using AngularAuthApi.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace AngularAuthApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly AppDbContext _authContext;
+        public UserController(AppDbContext appDbContext)
+        {
+            _authContext = appDbContext;    
+        }
+        [HttpPost("authenticate")]
+        public async Task<IActionResult> Authenticate([FromBody] User userobj)
+        {
+            if(userobj == null)
+                return BadRequest();
+
+            var user = await _authContext.Users
+                .FirstOrDefaultAsync(x => x.Username == userobj.Username && x.Password == userobj.Password);
+            if (user == null)
+                return NotFound(new { Message = "User not found!" });
+            return Ok(new
+            {
+                Message = "Login Success!"
+            });
+
+        }
+        [HttpPost("register")]
+        public async Task<IActionResult> RegisterUser([FromBody] User userobj)
+        {
+            if (userobj == null)
+                return BadRequest();
+                    await _authContext.Users.AddAsync(userobj);
+            await _authContext.SaveChangesAsync();
+            return Ok(new
+            {
+                Message = "User register Successfully!"
+            });
+        }
+
+    }
+}
